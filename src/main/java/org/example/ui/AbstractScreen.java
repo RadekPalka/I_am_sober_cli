f59bc9842cd0d388d1edc4422a72d445e41f39c3
Radosław Pałka <radekpalka@gmail.com>
2025-06-29 18:09:24 +0200

    feat: move HomeScreen methods to AbstractScreen

--- src/main/java/org/example/ui/AbstractScreen.java
+++ src/main/java/org/example/ui/AbstractScreen.java
@@ -1,7 +1,39 @@
 package org.example.ui;
 
+import org.example.context.AppContext;
+
 import java.util.List;
 
 public abstract class AbstractScreen implements Screen{
     protected MenuItem[] menu;
+    @Override
+    public void displayHeader(){
+        System.out.println("Welcome to I am sober cli app");
+    }
+
+    @Override
+    public void displayMenu(){
+        for (int i=0; i< menu.length; i++){
+            System.out.printf("%d-> %s%n", i+1, menu[i].getLabel());
+        }
+    }
+
+    @Override
+    public void getOptionFromUser() {
+        System.out.print("Choose option: ");
+        String userOption = AppContext.getInstance().getScanner().nextLine();
+
+        AppContext.getInstance().setUserOption(Integer.parseInt(userOption));
+    }
+
+    @Override
+    public void runMenuAction(int option){
+        menu[option-1].getAction().run();
+    }
+
+    @Override
+    public int getMenuArrayLength() {
+        return menu.length;
+    }
+
 }
--- src/main/java/org/example/ui/HomeScreen.java
+++ src/main/java/org/example/ui/HomeScreen.java
@@ -7,12 +7,13 @@ import java.util.List;
 
 public class HomeScreen extends AbstractScreen{
     private static HomeScreen instance;
-    private final MenuItem[] menu= {new MenuItem("Register", ()->System.out.println("You are registered")),
-            new MenuItem("Log in", ()-> System.out.println("You are login"))};
 
 
     private HomeScreen(){
-
+        this.menu = new MenuItem[]{
+                new MenuItem("Register", () -> System.out.println("You are registered")),
+                new MenuItem("Log in", () -> System.out.println("You are login"))
+        };
     }
 
     public static HomeScreen getInstance(){
@@ -22,35 +23,7 @@ public class HomeScreen extends AbstractScreen{
         }
         return instance;
     }
-    @Override
-    public void displayHeader(){
-        System.out.println("Welcome to I am sober cli app");
-    }
 
-    @Override
-    public void displayMenu(){
-        for (int i=0; i< menu.length; i++){
-            System.out.printf("%d-> %s%n", i+1, menu[i].getLabel());
-        }
-    }
-
-    @Override
-    public void getOptionFromUser() {
-        System.out.print("Choose option: ");
-        String userOption = AppContext.getInstance().getScanner().nextLine();
-
-        AppContext.getInstance().setUserOption(Integer.parseInt(userOption));
-    }
-
-    @Override
-    public void runMenuAction(int option){
-        menu[option-1].getAction().run();
-    }
-
-    @Override
-    public int getMenuArrayLength() {
-        return menu.length;
-    }
 
 
 
